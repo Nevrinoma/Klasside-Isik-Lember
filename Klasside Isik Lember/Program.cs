f52e8bbfc145e9d90527b559c4459e4eade55f8f
opilane <opilane@HP-CZC6297B9X>
2022-09-06 14:52:19 +0300

    want to cry 2

--- Klasside Isik Lember/Program.cs
+++ Klasside Isik Lember/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualBasic;
 using System;
 using static System.Formats.Asn1.AsnWriter;
 
+
 Console.WriteLine("----------------------------------Person information--------------------------------");
 Tootaja bob = new Tootaja("Bob", 2007, Isik.Sex.Male, Tootaja.Post.Clerk, "Microsoft", 989.50);
 bob.calSalary(500.0,20.0);
